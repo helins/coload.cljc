938a61987968d7481faf9ca27e640359c80e5cb7
Adam Helinski <adam@helins.io>
2021-04-09 22:16:24 +0200

    Annotate and document code

--- src/main/helins/coload.clj
+++ src/main/helins/coload.clj
@@ -5,7 +5,10 @@
 
 (ns helins.coload
 
-  ""
+  "Provides a Shadow-CLJS [[hook]] which synchronizes Clojure code with Clojurescript code in a
+   CLJC context.
+  
+   See README."
 
   {:author "Adam Helinski"}
 
@@ -24,7 +27,7 @@
 
 (defn- -state
 
-  ""
+  ;; Provides a new state for [[-*state]].
 
   []
 
@@ -35,7 +38,7 @@
 
 (def ^:private -*state
 
-  ;;
+  ;; State holding file tracker and watcher.
 
   (atom (-state)))
 
@@ -45,7 +48,7 @@
 
 (defn- -hawk-handler
 
-  ""
+  ;; Handler for Hawk watcher which updates the file tracker.
 
   [_hawk-ctx {:keys [file
                      kind]}]
@@ -56,7 +59,7 @@
          (fn [tracker]
            (delay
              ((if (identical? kind
-                             :delete)
+                              :delete)
                 clojure.tools.namespace.file/remove-files
                 clojure.tools.namespace.file/add-files)
               @tracker
@@ -67,7 +70,9 @@
 
 (defn- -hawk-clojure-file?
 
+  ;; Is the given file a Clojure file?
   ;;
+  ;; Important. For instance, a file editor might create temporary files.
 
   [_ctx {:keys [file]}]
 
@@ -79,7 +84,10 @@
 
 (defn- -exec-plugin-hook+
 
+  ;; For a given Shadow-CLJS compiling `stage`, executes a plugin function if its
+  ;; metadata request executation for that `stage`.
   ;;
+  ;; `param+` is the map provided to the plugin in the **shadow-cljs.edn** config file.
 
   [stage plugin+ param+]
 
@@ -116,7 +124,7 @@
 
 (defn- -reload
 
-  ;;
+  ;; Process of unloading and reloading relevant namespaces.
 
   [tracker stage plugin+]
 
@@ -152,7 +160,8 @@
 
 (defn- -shadow-configure
 
-  ""
+  ;; During the **configure** compilation stage in Shadow-CLJS, setup a watcher for the files
+  ;; requested by plugins.
 
   [plugin+]
 
@@ -208,7 +217,7 @@
 
 (defn- -shadow-compile-prepare
 
-  ""
+  ;; During the **compile-prepare** compilation stage in Shadow-CLJS
 
   [plugin+]
 
@@ -230,7 +239,9 @@
 
 (defn hook
 
-  ""
+  "Shadow-CLJS hook.
+  
+   See README."
 
   {:shadow.build/stages #{:compile-finish
                           :compile-prepare
@@ -264,7 +275,10 @@
 
 (defn clear!
 
-  ""
+  "Clear the current tracker.
+  
+   In other words, all watched files will be reloaded during next recompilation, whether they were
+   modified or not."
 
   []
 
@@ -288,7 +302,9 @@
 
 (defn compile-cycle
 
-  ""
+  "Returns the number designating the current compile cycle.
+  
+   Increments on each cycle."
 
   []
 
